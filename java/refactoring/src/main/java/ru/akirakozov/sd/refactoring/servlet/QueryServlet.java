203df9191dfe9662836fd7c91d5a1922ba8de0e7
Sofya Povarova <sofya.povarowa@gmail.com>
2021-01-26 17:42:16 +0300

    changed if on switch

--- java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
+++ java/refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/QueryServlet.java
@@ -25,24 +25,31 @@ public class QueryServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String command = request.getParameter("command");
 
-        if ("max".equals(command)) {
-            String header = "<h1>Product with max price: </h1>";
-            String res = getResponseFromDataBase(DB_URL, MAX_FROM_PRODUCT_TABLE);
-            responseWriter.writeHtmlResponse(response, header, res);
-        } else if ("min".equals(command)) {
-            String header = "<h1>Product with min price: </h1>";
-            String res = getResponseFromDataBase(DB_URL, MIN_FROM_PRODUCT_TABLE);
-            responseWriter.writeHtmlResponse(response, header, res);
-        } else if ("sum".equals(command)) {
-            String header = "Summary price: ";
-            String res = getResponseFromDataBase(DB_URL, SUM_PRICES_FROM_PRODUCT_TABLE);
-            responseWriter.writeHtmlResponse(response, header, res);
-        } else if ("count".equals(command)) {
-            String header = "Number of products: ";
-            String res = getResponseFromDataBase(DB_URL, COUNT_PRODUCT_TABLE);
-            responseWriter.writeHtmlResponse(response, header, res);
-        } else {
-            responseWriter.writeUnknownResponse(response, command);
+        String header;
+        String res;
+        switch (command) {
+            case ("max"):
+                header = "<h1>Product with max price: </h1>";
+                res = getResponseFromDataBase(DB_URL, MAX_FROM_PRODUCT_TABLE);
+                responseWriter.writeHtmlResponse(response, header, res);
+                break;
+            case ("min"):
+                header = "<h1>Product with min price: </h1>";
+                res = getResponseFromDataBase(DB_URL, MIN_FROM_PRODUCT_TABLE);
+                responseWriter.writeHtmlResponse(response, header, res);
+                break;
+            case ("sum"):
+                header = "Summary price: ";
+                res = getResponseFromDataBase(DB_URL, SUM_PRICES_FROM_PRODUCT_TABLE);
+                responseWriter.writeHtmlResponse(response, header, res);
+                break;
+            case ("count"):
+                header = "Number of products: ";
+                res = getResponseFromDataBase(DB_URL, COUNT_PRODUCT_TABLE);
+                responseWriter.writeHtmlResponse(response, header, res);
+                break;
+            default:
+                responseWriter.writeUnknownResponse(response, command);
         }
 
         responseWriter.endUpHtmlResponse(response);
